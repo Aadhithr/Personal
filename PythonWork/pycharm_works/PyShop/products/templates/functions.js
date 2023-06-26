eaebda1434c0504b90f61539bc5bc142918e1489
Aadhith Rajinikanth <aadhithr18@gmail.com>
2023-06-26 14:02:00 -0700

    organized

---
+++ PythonWork/pycharm_works/PyShop/products/templates/functions.js
@@ -0,0 +1,13 @@
+
+
+var name = new Array();
+var price = new Array();
+
+function storeData(x,y) {
+
+    name.push(x);
+    price.push(y);
+    return name
+    return price
+}
+
---
+++ PythonWork/pycharm_works/PyShop/products/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from .models import Product
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def products(request):
+    products = Product.objects.all()
+    return render(request, 'products.html', {'products': products})
+
+
+def buy(request):
+    products = Product.objects.all()
+    return render(request, 'buy.html', {'products': products})
